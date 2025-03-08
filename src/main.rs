b8c084d43667f8c91a65b33ea2e982746eba3181
signal32 <hdweir@outlook.com>
2025-03-08 18:19:09 +0000

    best game ever

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, bird_tweet_sys)
+        .add_systems(Startup, bird_add_sys)
+        .run();
+}
+
+#[derive(Component)]
+struct Bird {
+    name: String
+}
+
+impl Bird {
+    fn tweet(&self) {
+        println!("tweet i am a {}", self.name)
+    }
+}
+
+fn bird_tweet_sys(birds: Query<&Bird>) {
+    for bird in birds.iter() {
+        bird.tweet();
+    }
+}
+
+fn bird_add_sys(mut cmd: Commands) {
+    cmd.spawn(Bird { name: "greenfinch".to_string() });
 }
