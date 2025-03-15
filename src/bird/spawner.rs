3fd974949d6a3e2efd3389f96bb0f04d3de3f706
signal32 <hdweir@outlook.com>
2025-03-15 08:32:33 +0000

    add level spawn probability and cooldown

--- src/bird/spawner.rs
+++ src/bird/spawner.rs
@@ -10,7 +10,7 @@ use super::asset::BirdAssetHandle;
 #[derive(Component)]
 pub struct BirdSpawner {
     cooldown: f32,
-    spawn_probability: f64,
+    spawn_probability: f32,
 }
 
 /// Spawns birds from spawners.
@@ -32,7 +32,7 @@ pub(super) fn bird_spawn_sys(
             .entry(entity)
             .or_insert(time.elapsed_secs());
         let cooldown_expired = spawner.cooldown < time_now - *last_spawn_time;
-        let do_we_bird_yet = rng.random_bool(spawner.spawn_probability);
+        let do_we_bird_yet = rng.random_bool(spawner.spawn_probability as f64);
 
         if cooldown_expired && do_we_bird_yet {
             last_entity_spawn_time.insert(entity, time_now);
@@ -60,26 +60,41 @@ pub(super) fn bird_spawn_sys(
 /// Spawns bird spawners.
 pub(super) fn setup_spawner_sys(
     mut cmd: Commands,
-    windows: Query<&Window>
+    windows: Query<&Window>,
+    mut asset_ev: EventReader<AssetEvent<LevelAsset>>,
+    level_assets: Res<Assets<LevelAsset>>,
 ) {
     let window_height = windows.single().height();
     let window_width = windows.single().width();
     let bird_count = 10;
     let bird_padding = 200.;
 
-    for i in 0..bird_count {
-        let x = ((window_width - bird_padding) / (bird_count - 1) as f32) * i as f32;
+    for ev in asset_ev.read() {
+        match ev {
+            AssetEvent::LoadedWithDependencies { id } => {
+                let level = level_assets.get(*id).expect("No level");
 
-        let mut transform = Transform::from_xyz(
-            x - (window_width - bird_padding) * 0.5 ,
-            (window_height * 0.5) + 100.,
-            50.
-        );
-        transform.rotate_local_x(PI);
+                for i in 0..bird_count {
+                    let x = ((window_width - bird_padding) / (bird_count - 1) as f32) * i as f32;
 
-        cmd.spawn((
-            BirdSpawner { spawn_probability: 0.001, cooldown: 2. },
-            transform,
-        ));
-    }
+                    let mut transform = Transform::from_xyz(
+                        x - (window_width - bird_padding) * 0.5 ,
+                        (window_height * 0.5) + 100.,
+                        50.
+                    );
+                    transform.rotate_local_x(PI);
+                    cmd.spawn((
+                        BirdSpawner {
+                            spawn_probability: level.spawn_probability,
+                            cooldown: level.spawn_cooldown,
+                        },
+                        transform,
+                    ));
+                }
+            },
+            // todo: handle despawn of level entities
+            _ => ()
+        };
+     }
+
 }
