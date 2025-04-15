67de0c70fbae8ec7214a8448fe0b733a1f9c0fa6
signal32 <hdweir@outlook.com>
2025-04-15 08:17:28 +0100

    Add scenery

--- src/level.rs
+++ src/level.rs
@@ -2,16 +2,18 @@ use std::path::PathBuf;
 
 use bevy::{
     color::palettes::css::{GREEN, ORANGE},
-    prelude::*
+    prelude::*,
 };
 use serde::Deserialize;
 
 use crate::{
-    physics::{Collider, ColliderContactEvent, ColliderIntersectionMode, ColliderStatic}, util::ron_asset_loader::RonAssetLoader, GameState
+    GameState,
+    physics::{Collider, ColliderContactEvent, ColliderIntersectionMode, ColliderStatic},
+    util::ron_asset_loader::RonAssetLoader,
 };
 
 pub struct LevelPlugin {
-    pub default_level: PathBuf
+    pub default_level: PathBuf,
 }
 
 impl Default for LevelPlugin {
@@ -22,16 +24,13 @@ impl Default for LevelPlugin {
 
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .init_asset::<LevelAsset>()
+        app.init_asset::<LevelAsset>()
             .init_asset_loader::<RonAssetLoader<LevelAsset>>()
-            .insert_resource(Level {
-                default_level_path: self.default_level.clone(),
-                ..default()
-            })
+            .insert_resource(Level { default_level_path: self.default_level.clone(), ..default() })
+            .add_event::<LevelEvent>()
             .add_systems(Startup, setup_level_plugin_sys)
             .add_systems(OnEnter(GameState::Game), load_level_sys)
-            .add_systems(OnEnter(GameState::Menu), unload_level_sys)
+            // .add_systems(OnEnter(GameState::Menu), unload_level_sys)
             .add_systems(FixedUpdate, (on_level_load_sys, despawn_entities));
     }
 }
@@ -59,13 +58,14 @@ pub struct LevelAsset {
     pub birds: Vec<LevelBird>,
     pub players: Vec<LevelPlayer>,
     pub layers: Vec<LevelLayer>,
+    pub scenery: Vec<LevelScenery>,
 }
 
 /// Bird used in the level.
 #[derive(Debug, Deserialize)]
 pub struct LevelBird {
     pub asset: String,
-    pub spawn_probability: f32
+    pub spawn_probability: f32,
 }
 
 #[derive(Debug, Deserialize)]
@@ -81,17 +81,20 @@ pub struct LevelLayer {
     z: f32,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct LevelScenery {
+    image: PathBuf,
+    size: Vec2,
+    translation: Vec3,
+    collision: bool,
+}
+
 /// Wait for current level asset to load then setup game and transition to [GameState::Game] when ready.
-fn load_level_sys(
-    mut level: ResMut<Level>,
-    asset_server: Res<AssetServer>,
-) {
+fn load_level_sys(mut level: ResMut<Level>, asset_server: Res<AssetServer>) {
     level.level_handle = asset_server.load(level.default_level_path.clone());
 }
 
-fn unload_level_sys(
-    mut level: ResMut<Level>,
-) {
+fn unload_level_sys(mut level: ResMut<Level>) {
     level.level_handle = Handle::default();
 }
 
@@ -115,11 +118,12 @@ pub struct LevelRoot;
 /// They will then benefit from automatic cleanup along with the level.
 ///
 /// It is assumed that only one [LevelRoot] exists.
-pub type LevelRootEntity<'a> =  Single<'a, Entity, With<LevelRoot>>;
+pub type LevelRootEntity<'a> = Single<'a, Entity, With<LevelRoot>>;
 
 fn on_level_load_sys(
     mut cmd: Commands,
     mut level_asset_evts: EventReader<AssetEvent<LevelAsset>>,
+    mut level_evtw: EventWriter<LevelEvent>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut level: ResMut<Level>,
@@ -132,6 +136,10 @@ fn on_level_load_sys(
         match evt {
             AssetEvent::LoadedWithDependencies { id } => {
                 let level_asset = levels.get(*id).expect("Level should exist");
+                let mut root_cmds = cmd.entity(*root);
+
+                // Make sure any old level entities have been removed
+                root_cmds.try_despawn_descendants();
 
                 // Reset level
                 level.score = 0;
@@ -142,7 +150,7 @@ fn on_level_load_sys(
                 // Hit boxes to prevent player leaving play area
                 let play_area_hit_boxes = enclosing_rectangles(width, height);
                 for (rect, tf) in play_area_hit_boxes {
-                    cmd.entity(*root).with_child((
+                    root_cmds.with_child((
                         Collider::Rectangle(rect),
                         ColliderIntersectionMode::None,
                         ColliderStatic,
@@ -155,7 +163,7 @@ fn on_level_load_sys(
                 // Hit boxes to trigger despairing of entities that have left the play area
                 let despawn_area_hit_boxes = enclosing_rectangles(width * 2., height * 3.);
                 for (rect, tf) in despawn_area_hit_boxes {
-                    cmd.entity(*root).with_child((
+                    root_cmds.with_child((
                         Despawner,
                         Collider::Rectangle(rect),
                         ColliderIntersectionMode::None,
@@ -168,12 +176,17 @@ fn on_level_load_sys(
 
                 // Spawn layers
                 for layer in &level_asset.layers {
-                    cmd.entity(*root).with_child((
+                    root_cmds.with_child((
                         Sprite {
                             image: asset_server.load(layer.image.clone()),
                             image_mode: if layer.tiled {
-                                SpriteImageMode::Tiled { tile_x: true, tile_y: true, stretch_value: 2. }
-                            } else {
+                                SpriteImageMode::Tiled {
+                                    tile_x: true,
+                                    tile_y: true,
+                                    stretch_value: 2.,
+                                }
+                            }
+                            else {
                                 SpriteImageMode::Auto
                             },
                             custom_size: Some(Vec2::new(width, height)),
@@ -183,14 +196,44 @@ fn on_level_load_sys(
                     ));
                 }
 
-            },
-            AssetEvent::Unused { .. } => {
+                // Spawn scenery
+                for scenery in &level_asset.scenery {
+                    root_cmds.with_children(|builder| {
+                        let mut scenery_cmds = builder.spawn((
+                            Transform::from_translation(scenery.translation),
+                            Sprite {
+                                image: asset_server.load(scenery.image.clone()),
+                                custom_size: Some(scenery.size),
+                                ..default()
+                            },
+                        ));
+
+                        if scenery.collision {
+                            scenery_cmds.insert((
+                                Collider::Rectangle(Rectangle::from_size(scenery.size)),
+                                ColliderIntersectionMode::None,
+                                ColliderStatic,
+                            ));
+                        }
+                    });
+                }
+
+                level_evtw.send(LevelEvent::Loaded { id: *id });
+            }
+            AssetEvent::Unused { id } => {
                 info!("Clearing up level");
+                level_evtw.send(LevelEvent::Unloaded { id: *id });
                 cmd.entity(*root).try_despawn_descendants();
             }
             _ => (),
         }
     }
+}
+
+#[derive(Debug, Event)]
+pub enum LevelEvent {
+    Loaded { id: AssetId<LevelAsset> },
+    Unloaded { id: AssetId<LevelAsset> },
 }
 
 fn enclosing_rectangles(width: f32, height: f32) -> Vec<(Rectangle, Vec3)> {
@@ -201,8 +244,8 @@ fn enclosing_rectangles(width: f32, height: f32) -> Vec<(Rectangle, Vec3)> {
     vec![
         (Rectangle::new(width, bb_size), Vec3::new(0., h_height, 0.)),
         (Rectangle::new(width, bb_size), Vec3::new(0., -h_height, 0.)),
-        (Rectangle::new(bb_size, height + bb_size * 2.), Vec3::new(h_width, 0. , 0.)),
-        (Rectangle::new(bb_size, height + bb_size * 2.), Vec3::new(-h_width, 0. , 0.)),
+        (Rectangle::new(bb_size, height + bb_size * 2.), Vec3::new(h_width, 0., 0.)),
+        (Rectangle::new(bb_size, height + bb_size * 2.), Vec3::new(-h_width, 0., 0.)),
     ]
 }
 
