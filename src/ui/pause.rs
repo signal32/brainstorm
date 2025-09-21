9b217a06cc143d5c4f97d1a07c491e18ccf9bafa
Maisalae <62596769+ybettles@users.noreply.github.com>
2025-09-21 17:28:36 +0100

    Fix indent again

--- src/ui/pause.rs
+++ src/ui/pause.rs
@@ -7,17 +7,17 @@ impl Plugin for PausePlugin {
     fn build(&self, app: &mut App) {
         app
         .init_state::<PauseState>()
-            .add_systems(OnEnter(GameState::Pause), pause_setup_sys)
-            .add_systems(OnExit(GameState::Pause), despawn_entities::<OnMenuScreen>)
-            .add_systems(OnEnter(PauseState::PauseMenu), pause_menu_setup_sys)
-            .add_systems(OnExit(PauseState::PauseMenu), despawn_entities::<OnPauseMenuScreen>)
-            .add_systems(OnEnter(PauseState::Settings), settings_menu_setup_sys)
-            .add_systems(OnExit(PauseState::Settings), despawn_entities::<OnSettingsMenuScreen>)
-            .add_systems(Update, (
-                button_color_sys,
-                pause_button_action_sys,
-                pause_menu_listener_sys
-            ).run_if(in_state(GameState::Pause))
+        .add_systems(OnEnter(GameState::Pause), pause_setup_sys)
+        .add_systems(OnExit(GameState::Pause), despawn_entities::<OnMenuScreen>)
+        .add_systems(OnEnter(PauseState::PauseMenu), pause_menu_setup_sys)
+        .add_systems(OnExit(PauseState::PauseMenu), despawn_entities::<OnPauseMenuScreen>)
+        .add_systems(OnEnter(PauseState::Settings), settings_menu_setup_sys)
+        .add_systems(OnExit(PauseState::Settings), despawn_entities::<OnSettingsMenuScreen>)
+        .add_systems(Update, (
+            button_color_sys,
+            pause_button_action_sys,
+            pause_menu_listener_sys
+        ).run_if(in_state(GameState::Pause))
         );
     }
 }
